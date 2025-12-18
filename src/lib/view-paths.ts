4dc694d4e26b9363bc33ed4ee7ba61c1f878b716
Sumanth Chinthagunta <xmlking@gmail.com>
2025-12-17 16:33:24 -0800

    fix: added team update/delete dialogs

--- src/lib/view-paths.ts
+++ src/lib/view-paths.ts
@@ -45,6 +45,8 @@ export const organizationViewPaths = {
     SETTINGS: "settings",
     /** @default "members" */
     MEMBERS: "members",
+    /** @default "members" */
+    TEAMS: "teams",
     /** @default "api-keys" */
     API_KEYS: "api-keys"
 }
